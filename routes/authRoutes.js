57f69781118a2bcce185473f0871d00b02a278f1
Nithin Thampi <nithin.thampi87@gmail.com>
2017-11-05 00:19:38 +0530

    Removed default route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,6 @@
 const passport = require("passport");
 
 module.exports = app => {
-  app.get("/", (req, res) => {
-    res.send({
-      hello: "world"
-    });
-  });
-
   app.get(
     "/auth/google",
     passport.authenticate("google", {
