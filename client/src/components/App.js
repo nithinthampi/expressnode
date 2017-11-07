1ec69e5ac4920ff0479e4b84ef9a5209a1ffddf6
Nithin Thampi <nthampi@walmart.com>
2017-11-07 20:07:49 +0530

    Modified redux fom logic

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -15,7 +15,7 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div>
+        <div className="container">
           <Header />
           <Route exact path="/" component={Landing} />
           <Route exact path="/surveys" component={Dashboard} />
--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,17 +1,70 @@
 import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
+import SurveyField from "./SurveyField";
+import { Link } from "react-router-dom";
+
+const FIELDS = [
+  {
+    label: "Survey Title",
+    name: "title",
+    noValueError: "Please enter a value"
+  },
+  {
+    label: "Email Subject",
+    name: "subject",
+    noValueError: "Please enter a value"
+  },
+  {
+    label: "Email Body",
+    name: "body",
+    noValueError: "Please enter a value"
+  },
+  {
+    label: "Recipients",
+    name: "recipients",
+    noValueError: "Please enter a value"
+  }
+];
 
 class SurveyForm extends Component {
+  renderFields() {
+    return FIELDS.map(({ label, name }, index) => (
+      <Field
+        type="input"
+        key={index}
+        component={SurveyField}
+        name={name}
+        label={label}
+      />
+    ));
+  }
+
   render() {
     return (
       <div>
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
-          <Field type="input" name="surveyTitle" component="input" />
-          <button type="submit">Submit</button>
+          {this.renderFields()}
+          <Link to="/surveys" className="red btn-flat white-text">
+            Cancel
+          </Link>
+          <button type="submit" className="teal btn-flat right white-text">
+            Submit
+            <i className="material-icons right">done</i>
+          </button>
         </form>
       </div>
     );
   }
 }
 
-export default reduxForm({ form: "SurveyForm" })(SurveyForm);
+function validate(values) {
+  const errors = {};
+  FIELDS.forEach(({ name, noValueError }) => {
+    if (!values[name]) {
+      errors[name] = noValueError;
+    }
+  });
+  return errors;
+}
+
+export default reduxForm({ validate, form: "SurveyForm" })(SurveyForm);
